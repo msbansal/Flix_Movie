50a20fc8e7e953dad2003d81aa4576f341953c31
Mahak Bansal <mahak.bansal97@gmail.com>
2020-09-19 19:09:57 -0700

    Initial Commit

---
+++ Flix Movie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix Movie
+//
+//  Created by Mahak Bansal on 9/19/20.
+//  Copyright © 2020 Mahak Bansal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
